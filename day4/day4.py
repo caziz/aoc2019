6d7227c8b8950e8e10581f666002e9e65cd24f3d
Christopher Aziz <caziz@ucla.edu>
2019-12-04 14:26:45 -0800

    Rewrite day4.py

--- day4/day4.py
+++ day4/day4.py
@@ -1,23 +1,26 @@
 part_1 = 0
 part_2 = 0
-for n in range(240298, 784956):
-    increasing = False
-    nums = []
-    counts = []
-    while n != 0:
-        d = n % 10
-        n //= 10
-        if nums and d > nums[-1]:
-            increasing = True
+for num in range(240298, 784956):
+    num_str = str(num)
+    decreasing = False
+    double = False
+    double_alone = False
+    for i, c1 in enumerate(num_str):
+        if i == 0:
+            continue
+        c0 = num_str[i - 1]
+        if c0 > c1:
+            decreasing = True
             break
-        if nums and d == nums[-1]:
-            counts[-1] += 1
-        else:
-            nums.append(d)
-            counts.append(1)
-    if not increasing and len(counts) != 6:
+        if c0 == c1:
+            double = True
+            front = i == 1 or num_str[i - 2] != c1
+            back = i == len(num_str) - 1 or num_str[i + 1] != c1
+            if front and back:
+                double_alone = True
+    if not decreasing and double:
         part_1 += 1
-        if 2 in counts:
+        if double_alone:
             part_2 += 1
 print(part_1)
 print(part_2)
